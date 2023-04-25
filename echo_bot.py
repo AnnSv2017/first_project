374320d8dba8ce9512c1b51672071a47366e3566
Ann Svn <x06254.bin@gmail.com>
2023-04-25 10:00:14 +0300

    echo_bot_ready

--- echo_bot.py
+++ echo_bot.py
@@ -2,39 +2,32 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
-# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
 API_TOKEN: str = '6140558942:AAEOFhwchO2_P3K8aGCB386dBbX0CQ28lJw'
 
-# Создаем объекты бота и диспетчера
 bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
 
 # Этот хэндлер будет срабатывать на команду "/start"
-
+@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 # Этот хэндлер будет срабатывать на команду "/help"
-
+@dp.message(Command(commands=["help"]))
 async def process_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь и в ответ '
                          'я пришлю тебе твое сообщение')
 
-# Этот хэндлер будет срабатывать на отправку боту фото
-async def send_photo_echo(message: Message):
-    print(message.json())
-    await message.reply_photo(message.photo[0].file_id)
-
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
 # кроме команд "/start" и "/help"
-
+@dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
-
-dp.message.register(process_start_command, Command(commands=['start']))
-dp.message.register(process_help_command, Command(commands=['help']))
-dp.message.register(send_photo_echo, F.photo)
-dp.message.register(send_echo)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+        print(message.json(indent=4, exclude_none=True))
+    except TypeError:
+        await message.reply(text='Данный тип апдейтов не поддерживается '
+                                 'методом send_copy')
 
 if __name__ == '__main__':
     dp.run_polling(bot)
