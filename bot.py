c5800f3037311186bbc88606e46d65d18e22abdc
Ann Svn <x06254.bin@gmail.com>
2023-05-14 16:54:45 +0300

    create modular_echo_bot

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from config import load_config
 
-config = load_config(".env")
+config = load_config(".env") # "./.env" Путь к основному окружению
 
 bot_token = config.tg_bot.token           # Сохраняем токен в переменную bot_token
 superadmin = config.tg_bot.admin_ids[0]   # Сохраняем ID админа в переменную superadmin
