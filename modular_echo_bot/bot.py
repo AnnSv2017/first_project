519bf7910390062bdb0c8e7fbc8f5c3a14740d9a
Ann Svn <x06254.bin@gmail.com>
2023-05-17 19:06:19 +0300

    updates

--- modular_echo_bot/bot.py
+++ modular_echo_bot/bot.py
@@ -1,10 +1,11 @@
 import asyncio
+
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
-
+from handlers import user_handlers, other_handlers
 
 # Функция конфигурирования и запуска бота
-async def main() -> None:
+async def main():
 
     # Загружаем конфиг в переменную config
     config: Config = load_config()
@@ -13,9 +14,15 @@ async def main() -> None:
     bot: Bot = Bot(token=config.tg_bot.token)
     dp: Dispatcher = Dispatcher()
 
+    # Регистриуем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
+
 if __name__ == '__main__':
     asyncio.run(main())
+
